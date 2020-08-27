46ffa67e3eb76c2b273ee0beca7644c5c5b03b2c
luis-rios <vvrhz11@gmail.com>
2020-08-26 20:28:05 -0500

    'filtros_paginacion'

--- newsletters/views.py
+++ newsletters/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from newsletters.models import Newsletter
-from newsletters.serializers import NewsletterSerializer
+from newsletters.serializers import NewsletterSerializer, BaseNewsletterSerializer
 
 
 class NewsletterViewSet(viewsets.ModelViewSet):
     queryset = Newsletter.objects.all()
     serializer_class = NewsletterSerializer
 
+    def get_queryset(self):
+        parameters = {}
+        for param in self.request.query_params:
+            if param in ['page', 'page_size']:
+                continue
+            if param in ['tags', 'newsletter', 'id']:
+                continue
+            newsletter_filtered = Newsletter.objects.filter(**parameters)
+            return newsletter_filtered
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return BaseNewsletterSerializer
+        return NewsletterSerializer
--- tags/views.py
+++ tags/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from tags.models import Tag
-from tags.serializers import TagSerializer
+from tags.serializers import TagSerializer, BaseTagSerializer
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+
+    def get_queryset(self):
+        parameters = {}
+        for param in self.request.query_params:
+            if param in ['page', 'page_size']:
+                continue
+            if param in ['tags', 'newsletter', 'id']:
+                continue
+            tags_filtered = Tag.objects.filter(**parameters)
+            return tags_filtered
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return BaseTagSerializer
+        return TagSerializer
+
